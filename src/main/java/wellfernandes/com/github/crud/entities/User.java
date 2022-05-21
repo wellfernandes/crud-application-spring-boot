64de4e8cf364adeabbc82fcf75ba36d6b1f51c11
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-21 12:39:56 -0300

    order, instant and iso 8601 - p1

--- src/main/java/wellfernandes/com/github/crud/entities/User.java
+++ src/main/java/wellfernandes/com/github/crud/entities/User.java
@@ -1,14 +1,19 @@
 package wellfernandes.com.github.crud.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -75,6 +83,10 @@ public class User implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
