562c6659283afacf606c1f70d61799528cc6fe18
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-25 17:58:39 -0300

    exception handling and findbyid

---
+++ src/main/java/wellfernandes/com/github/crud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package wellfernandes.com.github.crud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found " + id);
+	}
+	
+}
